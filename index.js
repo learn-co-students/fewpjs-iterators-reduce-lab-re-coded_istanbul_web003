3dbbbad33df23642e7df7fe816fc734d8f18ca6a
Luey Monla Ahmed <67810739+Luey-A@users.noreply.github.com>
2020-10-28 21:19:08 +0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
-
-const totalBatteries = batteryBatches.reduce ((accumulator,currentValue) => accumulator + currentValue,0) 
