1be3b8198ce3ddd3591e4b913ed672eb8d37d2ff
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-28 18:13:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-const findMatching = (arr, str) => {
-  return arr.filter((e) => e.toLowerCase() === str.toLowerCase());
-};
 
-const fuzzyMatch = (arr, str) => {
-  return arr.filter((e) => e.slice(0, str.length) === str);
-};
-
-const matchName = (arr, str) => {
-  return arr.filter((e) => e.name === str);
-};
+const totalBatteries = batteryBatches.reduce ((accumulator,currentValue) => accumulator + currentValue,0) 
