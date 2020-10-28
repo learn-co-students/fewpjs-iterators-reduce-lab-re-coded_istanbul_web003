9897de2679e89e20c868222510f8debbd1de051b
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-28 14:44:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const findMatching = (arr, str) => {
+  return arr.filter((e) => e.toLowerCase() === str.toLowerCase());
+};
+
+const fuzzyMatch = (arr, str) => {
+  return arr.filter((e) => e.slice(0, str.length) === str);
+};
+
+const matchName = (arr, str) => {
+  return arr.filter((e) => e.name === str);
+};
